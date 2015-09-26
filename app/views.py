b061e8c349b08006f18be1ff31ffd6a8a1326c3e
Drew Galbraith <dgalbraith33@gmail.com>
2015-09-26 04:10:20 -0400

    Added views

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from app import app
 from .api import video
 
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 
 @app.route('/api/skip')
--- run.py
+++ run.py
@@ -1,3 +1,3 @@
-#!flask/bin/python
+#!virtenv/bin/python
 from app import app
 app.run(debug=True)
