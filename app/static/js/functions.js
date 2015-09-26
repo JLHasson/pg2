19032dd848fd63257b34141649649da5b361c10a
brentzucker <brentzucker@gmail.com>
2015-09-26 13:49:54 -0400

    Skip Button Loads New Video when Skip Threshold is met

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	/* Set Up Youtube IFrame Player */
+
 	// 2. This code loads the IFrame Player API code asynchronously.
 	var tag = document.createElement('script');
 
@@ -7,7 +9,8 @@ $(document).ready(function() {
 	var firstScriptTag = document.getElementsByTagName('script')[0];
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-	// Load On Click listers
+	/* Load On Click Listeners */
+
 	$('#skip-button').on("click", function() {
 		var skipURL = '/api/skip';
 		console.log("skip");
@@ -25,38 +28,79 @@ $(document).ready(function() {
 	    if ($('#skip-button').hasClass('btn-danger'))
 	        $('#skip-button').toggleClass("btn-danger");
 	});
+
+	// Ask Web Server, Do I need to update? (every 500ms)
+	setInterval(getCurrentVideo, 500);
 });
+
+function getCurrentVideo() {
+	var getVideoURL = '/api/get';
+	$.ajax({
+			type: "GET",
+			url: getVideoURL, 
+			success: updateYoutubeFrame
+	});
+}
+
+function updateYoutubeFrame(video_id) {
+	console.log("updateYoutubeFrame to: " + video_id);
+
+	// If youtubeFrameVideo is different than current video on Server
+	if (youtubeFrameVideoId != video_id) {
+		
+		console.log("Change Video to: " + video_id);
+		
+		player.loadVideoById({'videoId': video_id});
+
+		// Update Global Variable 
+		youtubeFrameVideoId = video_id;
+	}
+}
+
+// Used intially
+function createYoutubeFrame(video_id) {
+	
+	// Store in Global Variable
+	youtubeFrameVideoId = video_id;
+
+	player = new YT.Player('player', {
+			height: '390',
+			width: '640',
+			videoId: video_id,
+			events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+			}
+	});
+}
 
 /* Youtube API Functions */
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 function onYouTubeIframeAPIReady() {
-	player = new YT.Player('player', {
-			height: '390',
-			width: '640',
-			videoId: 'M7lc1UVf-VE',
-			events: {
-			'onReady': onPlayerReady,
-			'onStateChange': onPlayerStateChange
-		}
-	});
+	// Load Current Video
+	var getVideoURL = '/api/get';
+	$.ajax({
+			type: "GET",
+			url: getVideoURL, 
+			success: createYoutubeFrame
+		});
 }
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
 	event.target.playVideo();
+
+	// Check to see if you need to load a new VideoById
+	getCurrentVideo();
 }
 
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
-done = false;
 function onPlayerStateChange(event) {
-	if (event.data == YT.PlayerState.PLAYING && !done) {
-		setTimeout(stopVideo, 6000);
-		done = true;
-	}
+
 }
 
 function stopVideo() {
