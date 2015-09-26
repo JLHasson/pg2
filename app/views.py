1ae08a7bb74758731108b112a599cfe9bd7f67bb
Drew Galbraith <dgalbraith33@gmail.com>
2015-09-26 04:05:09 -0400

    Added initial api

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from app import app
+from .api import video
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+
+@app.route('/api/skip')
+def skip():
+    video.skipVideo()
+    return "Recorded"
+
+
+@app.route('/api/get')
+def get():
+    return video.getVideo()
