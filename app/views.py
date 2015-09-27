4f214e5d06428cbde5dbdc587a02ef659673b9f5
brentzucker <brentzucker@gmail.com>
2015-09-26 21:44:33 -0400

    Created Stats route and Stats Template

--- app/views.py
+++ app/views.py
@@ -22,6 +22,9 @@ def get_ip():
 def index():
     return render_template('index.html')
 
+@app.route('/stats')
+def stats():
+    return render_template('stats.html')
 
 @app.route('/api/skip')
 def skip():
