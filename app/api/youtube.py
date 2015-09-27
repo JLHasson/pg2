20d598e10b0255b3bc1de399ca130c768177fa2a
Drew Galbraith <dgalbraith33@gmail.com>
2015-09-27 09:31:16 -0400

    Removed API Key, moved to settings

--- app/api/youtube.py
+++ app/api/youtube.py
@@ -6,13 +6,13 @@ import logging
 import random
 logging.getLogger("googleapiclient.discovery").setLevel(logging.WARNING)
 logging.getLogger("googleapiclient.discovery_cache.file_cache").setLevel(logging.WARNING)
-
+from app import app
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyBsn-BYrt_Rv41zb2AzRdwGjEOgpYUFs-E"
+DEVELOPER_KEY = app.config["YOUTUBE_API_KEY"]
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
